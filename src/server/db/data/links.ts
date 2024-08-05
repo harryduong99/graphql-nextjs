7728e20d72914fd6b277103ab30b3a2dbbdd1bef
harry-nutsfi <harry@nuts.finance>
2024-08-05 22:00:49 +0700

    feat: add prisma postgres with migration and seed

---
+++ src/server/db/data/links.ts
@@ -0,0 +1,16 @@
+export const links = [
+  {
+    category: "Open Source",
+    description: "Fullstack React framework",
+    imageUrl: "https://nextjs.org/static/twitter-cards/home.jpg",
+    title: "Next.js",
+    url: "https://nextjs.org",
+  },
+  {
+    category: "Open Source",
+    description: "Next Generation ORM for TypeScript and JavaScript",
+    imageUrl: "https://www.prisma.io/images/og-image.png",
+    title: "Prisma",
+    url: "https://prisma.io",
+  },
+];
