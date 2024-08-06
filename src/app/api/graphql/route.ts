60fea0f175e8b8d83d07a8a722e905db93755773
harry-nutsfi <harry@nuts.finance>
2024-08-06 10:29:10 +0700

    fix: schema and resolver with hardcoded data

--- src/app/api/graphql/route.ts
+++ src/app/api/graphql/route.ts
@@ -1,8 +1,14 @@
 import { createSchema, createYoga } from "graphql-yoga";
 import { schema } from "../../../server/graphql/schema";
+import { typeDefs } from "../../../server/graphql/schema";
+import { resolvers } from "../../../server/graphql/resolver";
 
 const { handleRequest } = createYoga({
-  schema: schema,
+  // schema: schema,
+  schema: createSchema({
+    typeDefs,
+    resolvers,
+  }),
 
   // While using Next.js file convention for routing, we need to configure Yoga to use the correct endpoint
   graphqlEndpoint: "/api/graphql",
--- src/server/graphql/schema.ts
+++ src/server/graphql/schema.ts
@@ -10,4 +10,20 @@ builder.queryType({
   }),
 });
 
+export const typeDefs = `
+  type Link {
+    id: ID
+    title: String
+    description: String
+    url: String
+    category: String
+    imageUrl: String
+    users: [String]
+  }
+
+  type Query {
+    links: [Link]!
+  }
+`;
+
 export const schema = builder.toSchema();
