6177edc1c7cec93b2bdc42afc994e444e20d7467
harryduong99 <duongminhnam99@gmail.com>
2024-08-07 11:57:13 +0700

    fix: update graphql code-gen to use schema directly instead of api endpoint

--- codegen.ts
+++ codegen.ts
@@ -1,7 +1,9 @@
 import type { CodegenConfig } from "@graphql-codegen/cli";
+import { schema } from "./src/server/graphql/schema";
+import { printSchema } from "graphql";
 
 const config: CodegenConfig = {
-  schema: "http://localhost:3000/api/graphql",
+  schema: printSchema(schema), // available format: URL, JSON, code file (.ts), JS export (.js), String, Git...
   documents: ["src/**/*.graphql"],
   generates: {
     "./src/client/gql/": {
